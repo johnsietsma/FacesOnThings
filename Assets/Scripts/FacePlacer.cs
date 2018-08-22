8f5ec0fc68456e06565bbc071c175934be5ba000
John Sietsma <john.sietsma@gmail.com>
2018-08-22 19:00:01 +1000

    Work on getting better face alignment.

--- Assets/Scripts/FacePlacer.cs
+++ Assets/Scripts/FacePlacer.cs
@@ -37,10 +37,21 @@ public class FacePlacer : MonoBehaviour
 
             if (m_SessionOrigin.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
             {
-                Debug.Log("Hit");
                 Pose hitPose = s_Hits[0].pose;
-                m_FacePlaneTransform.position = hitPose.position;
-                m_FacePlaneTransform.rotation = hitPose.rotation;
+                BoundedPlane plane = ARSubsystemManager.planeSubsystem.GetPlane(s_Hits[0].trackableId);
+                Pose planePose = plane.Pose;
+
+                m_FacePlaneTransform.position = hitPose.position + hitPose.up * 0.1f;
+
+                Vector3 towardsCam = (m_SessionOrigin.camera.transform.position - hitPose.position).normalized;
+                Vector3 right = -Vector3.Cross(towardsCam, planePose.forward); // Consider towardsCam to be close to up
+                Vector3 forward = Vector3.Cross(right, planePose.up);
+                m_FacePlaneTransform.rotation = Quaternion.LookRotation(forward, planePose.up);
+
+
+                //Debug.LogFormat("Hit Pose - {0} {1}", hitPose.rotation.eulerAngles, sessionPose.rotation.eulerAngles);
+                //Debug.LogFormat("Hit Pose - Up:{0:0.####} Fwd:{1:0.####} Right:{2:0.####}", hitPose.up, hitPose.forward, hitPose.right);
+                //Debug.LogFormat("ToCam:{0} Fwd:{1} Right:{2} Euler:{3:0.####}", towardsCam, forward, right, m_FacePlaneTransform.rotation.eulerAngles);
             }
         }
     }
---
+++ Assets/Scripts/XRPlaneSubsystemExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+
+public static class XRPlaneSubsystemExtensions {
+
+    private static List<BoundedPlane> s_BoundedPlanes = new List<BoundedPlane>();
+
+    public static BoundedPlane GetPlane( this XRPlaneSubsystem planeSubsystem, TrackableId planeId )
+    {
+        planeSubsystem.GetAllPlanes(s_BoundedPlanes);
+        foreach( var plane in s_BoundedPlanes )
+        {
+            if (plane.Id == planeId) return plane;
+        }
+        return new BoundedPlane();
+    }
+}
