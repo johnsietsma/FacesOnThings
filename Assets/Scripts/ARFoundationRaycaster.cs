3bf9f5666d215f71af97824cc3f15084850bd8a1
John Sietsma <john.sietsma@gmail.com>
2018-08-23 17:50:39 +1000

    Added ARFoundationRaycaster and moved over to use the event system for touch handling.

---
+++ Assets/Scripts/ARFoundationRaycaster.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+namespace UnityEngine.EventSystems
+{
+    /// <summary>
+    /// Simple event system using AR Foundation raycasts.
+    /// </summary>
+    [AddComponentMenu("Event/ARFoundation Raycaster")]
+    public class ARFoundationRaycaster : BaseRaycaster
+    {
+        [SerializeField]
+        ARSessionOrigin m_SessionOrigin;
+
+        /// <summary>
+        /// Layer mask used to filter events. Always combined with the camera's culling mask if a camera is used.
+        /// </summary>
+        [SerializeField]
+        TrackableType m_TrackableTypeMask = TrackableType.All;
+
+        private List<ARRaycastHit> m_raycastHits = new List<ARRaycastHit>();
+
+        public override Camera eventCamera
+        {
+            get { return arSessionOrigin.camera; }
+        }
+
+        public ARSessionOrigin arSessionOrigin
+        {
+            get
+            {
+                if (m_SessionOrigin == null)
+                    m_SessionOrigin = GetComponent<ARSessionOrigin>();
+                return m_SessionOrigin;
+            }
+        }
+
+        /// <summary>
+        /// Layer mask used to filter events. Always combined with the camera's culling mask if a camera is used.
+        /// </summary>
+        public TrackableType trackableTypeMask
+        {
+            get { return m_TrackableTypeMask; }
+            set { m_TrackableTypeMask = value; }
+        }
+
+
+        /// <summary>
+        /// The index of the RaycastResult can be used to find the trackableId.
+        /// Use TrackableId id = (int)eventData.pointerCurrentRaycast.index;
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public TrackableId GetTrackableId( int index)
+        {
+            if (index < m_raycastHits.Count) return m_raycastHits[index].trackableId;
+            return TrackableId.InvalidId;
+        }
+
+        public override void Raycast(PointerEventData eventData, List<RaycastResult> resultAppendList)
+        {
+            if (arSessionOrigin.Raycast(eventData.position, m_raycastHits, trackableTypeMask))
+            {
+                m_raycastHits.Sort((r1, r2) => r1.distance.CompareTo(r2.distance));
+
+                for (int i = 0; i < m_raycastHits.Count; i++)
+                {
+                    var hit = m_raycastHits[i];
+                    resultAppendList.Add(
+                        new RaycastResult()
+                        {
+                            gameObject = gameObject, // TODO Generic find trackables?
+                            module = this,
+                            distance = hit.distance,
+                            worldPosition = hit.pose.position,
+                            worldNormal = hit.pose.up,
+                            screenPosition = eventData.position,
+                            index = i,
+                            sortingLayer = 0,
+                            sortingOrder = 0
+                        }
+                    );
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/FacePlacer.cs
+++ Assets/Scripts/FacePlacer.cs
@@ -5,7 +5,7 @@ using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
 
 [RequireComponent(typeof(ARSessionOrigin))]
-public class FacePlacer : MonoBehaviour
+public class FacePlacer : MonoBehaviour, IPointerDownHandler
 {
     [SerializeField]
     Transform m_FacePlaneTransform;
@@ -32,27 +32,45 @@ public class FacePlacer : MonoBehaviour
             if (EventSystem.current && EventSystem.current.IsPointerOverGameObject()) return;
 
             Vector3 touchPosition;
-            if( Input.touchCount > 0 ) touchPosition = Input.GetTouch(0).position;
+            if (Input.touchCount > 0) touchPosition = Input.GetTouch(0).position;
             else touchPosition = Input.mousePosition;
 
             if (m_SessionOrigin.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
             {
-                Pose hitPose = s_Hits[0].pose;
-                BoundedPlane plane = ARSubsystemManager.planeSubsystem.GetPlane(s_Hits[0].trackableId);
-                Pose planePose = plane.Pose;
-
-                m_FacePlaneTransform.position = hitPose.position + hitPose.up * 0.1f;
-
-                Vector3 towardsCam = (m_SessionOrigin.camera.transform.position - hitPose.position).normalized;
-                Vector3 right = -Vector3.Cross(towardsCam, planePose.forward); // Consider towardsCam to be close to up
-                Vector3 forward = Vector3.Cross(right, planePose.up);
-                m_FacePlaneTransform.rotation = Quaternion.LookRotation(forward, planePose.up);
-
-
-                //Debug.LogFormat("Hit Pose - {0} {1}", hitPose.rotation.eulerAngles, sessionPose.rotation.eulerAngles);
-                //Debug.LogFormat("Hit Pose - Up:{0:0.####} Fwd:{1:0.####} Right:{2:0.####}", hitPose.up, hitPose.forward, hitPose.right);
-                //Debug.LogFormat("ToCam:{0} Fwd:{1} Right:{2} Euler:{3:0.####}", towardsCam, forward, right, m_FacePlaneTransform.rotation.eulerAngles);
+                //Pose pose = s_Hits[0].pose;
+                //BoundedPlane plane = ARSubsystemManager.planeSubsystem.GetPlane(s_Hits[0].trackableId);
+                //PlaceFace( pose.position, pose.up, plane.Pose);
             }
         }
+    }
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        var arFoundationModule = eventData.pointerCurrentRaycast.module as ARFoundationRaycaster;
+        if (arFoundationModule == null) return;
+
+        var currentRaycast = eventData.pointerCurrentRaycast;
+
+        int trackableIndex = (int)currentRaycast.index;
+        TrackableId trackableId = arFoundationModule.GetTrackableId(trackableIndex);
+        BoundedPlane plane = ARSubsystemManager.planeSubsystem.GetPlane(trackableId);
+
+        PlaceFace(currentRaycast.worldPosition, currentRaycast.worldNormal, plane.Pose);
+    }
+
+    private void PlaceFace( Vector3 position, Vector3 normal, Pose planePose )
+    {
+        m_FacePlaneTransform.position = position + normal * 0.1f;
+
+        Vector3 towardsCam = (m_SessionOrigin.camera.transform.position - position).normalized;
+        Vector3 right = -Vector3.Cross(towardsCam, planePose.forward);
+        Vector3 forward = Vector3.Cross(right, planePose.up);
+        m_FacePlaneTransform.rotation = Quaternion.LookRotation(forward, planePose.up);
+
+
+        //Debug.LogFormat("Hit Pose - {0} {1}", hitPose.rotation.eulerAngles, sessionPose.rotation.eulerAngles);
+        //Debug.LogFormat("Hit Pose - Up:{0:0.####} Fwd:{1:0.####} Right:{2:0.####}", hitPose.up, hitPose.forward, hitPose.right);
+        //Debug.LogFormat("ToCam:{0} Fwd:{1} Right:{2} Euler:{3:0.####}", towardsCam, forward, right, m_FacePlaneTransform.rotation.eulerAngles);
     }
 }
